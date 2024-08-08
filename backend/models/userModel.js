72a6de72fda5d41d8d4c2e2315e71d3a772e7435
mattcoco <matiascontr@gmail.com>
2024-08-08 15:53:43 +0200

    7.6 - OK

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,6 +31,18 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// Lo que tenga que ver con el userSchema se ejecutará antes de guardar el usuario en la base de datos
+userSchema.pre("save", async function (next) {
+  // Si el password no ha sido modificado, no hacemos nada
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  // Si el password ha sido modificado, lo encriptamos
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model("User", userSchema);
 
 export default User;

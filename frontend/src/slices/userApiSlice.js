ed19767ffc42b326e90a7c1a83f7a4164599ddad
mattcoco <matiascontr@gmail.com>
2024-08-09 11:52:22 +0200

    8.5 - OK - Frontend Authentication

--- frontend/src/slices/userApiSlice.js
+++ frontend/src/slices/userApiSlice.js
@@ -13,6 +13,14 @@ export const usersApiSlice = apiSlice.injectEndpoints({
             }),
         }),
 
+        register: builder.mutation({
+            query: (data) => ({
+                url: `${USERS_URL}`,
+                method: "POST",
+                body: data
+            }),
+        }),
+
         logout: builder.mutation({
             query: () => ({
                 url: `${USERS_URL}/logout`,
@@ -22,4 +30,4 @@ export const usersApiSlice = apiSlice.injectEndpoints({
     }),
 });
 
-export const { useLoginMutation, useLogoutMutation } = usersApiSlice;
+export const { useLoginMutation, useLogoutMutation, useRegisterMutation } = usersApiSlice;
